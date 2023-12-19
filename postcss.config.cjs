d067279c1146b0f8b00f08a4b3e850a37e3be98b
Daniel Pencak <danielpencak@gmail.com>
2023-12-18 19:37:32 -0600

    Cleanup

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,5 +3,5 @@ const tailwindcss = require('tailwindcss');
 const autoprefexir = require('autoprefixer');
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.cjs'), autoprefexir]
-}
+	plugins: [tailwindcss('./tailwind.config.cjs'), autoprefexir],
+};
