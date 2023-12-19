99338ba5d593d7d0fd0b7d01af87aae5997cdea2
Daniel Pencak <danielpencak@gmail.com>
2023-12-18 19:22:00 -0600

    Setup Tailwind

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const tailwindcss = require('tailwindcss');
+const autoprefexir = require('autoprefixer');
+
+module.exports = {
+  plugins: [tailwindcss('./tailwind.config.cjs'), autoprefexir]
+}
